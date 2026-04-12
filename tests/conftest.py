159c1069c3e93850ced3f12d474c7a9537f9a1fa
Alexander Rozanov <al.rozanov@innopolis.university>
2026-04-12 20:24:32 +0300

    FIX

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ _cf_zero_width = "your friend\u200B"             # ZERO‑WIDTH SPACE (Cf)
 
 
 ESOTERIC_STRINGS = [_cc_cstring, _cf_left_to_right, _cf_right_to_left,
-                    _cf_zero_width, _co_private, _cs_low, _co_str, _cs_str]
+                    _cf_zero_width]
 WRONG_CONTENT_TYPES = ["text/html", "text/plain", "text/css", "text/javascript",
                           "application/xml", "application/pdf", "image/png", "image/jpeg",
                           "audio/mpeg", "video/mp4", "multipart/form-data"]
